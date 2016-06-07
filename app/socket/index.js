a1fe7e1392f8a3348d5f3615493f14a5eb509223
Chris Kokolios <kokolios1986@yahoo.gr>
2016-06-07 23:16:25 +0300

    playing with socket io

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,10 +1,24 @@
 'use strict';
 
 
-module.exports = (io,app) => {
-	let allrooms = app.locals.chatrooms;
-	io.of('/roomslist').on('connection',socket => {
-		console.log('hey')
-	});
+module.exports = (io, app) => {
+    let allrooms = app.locals.chatrooms;
+    allrooms.push({
+        room: 'Good Food',
+        roomID: '0001',
+        users: []
+    });
+    allrooms.push({
+        room: "Papa",
+        roomID: '0002',
+        users: []
+    });
+    io.of('/roomslist').on('connection', socket => {
+        socket.on('getChatrooms', () => {
+
+            socket.emit('chatRoomsList', JSON.stringify(allrooms))
+
+        });
+    });
 
 }
